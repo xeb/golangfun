eb251d3d583f58b93f4ce51b4e606f321feccfd9
Mark Kockerbeck <mark@kockerbeck.com>
2013-08-28 23:43:56 -0700

    It kinda works but I dont really like Reveal for this now.  I think I should split things out into their own pages.

--- presentation/server.go
+++ presentation/server.go
@@ -3,11 +3,12 @@ package presentation
 import (
 	"fmt"
 	"html/template"
+	"io"
+	"io/ioutil"
 	"net/http"
+	"net/url"
 )
 
-// ridiculously long path name!
-// var templatePath = "/Users/xeb/Dropbox/Projects/GoCode/src/github.com/xeb/golangfun/presentation/index.html"
 var templatePath = "./presentation/index.html"
 var port int = 8093
 
@@ -34,9 +35,10 @@ func handleRequest(w http.ResponseWriter, r *http.Request) {
 	switch url {
 	case "/":
 		handleRootReq(w)
+	case "/compile":
+		handleCompile(w, r)
 	default:
 		contentMux.ServeHTTP(w, r)
-		// fmt.Fprintf(w, "No handler for %s", url)
 	}
 }
 
@@ -53,4 +55,50 @@ func handleRootReq(w http.ResponseWriter) {
 		fmt.Fprintf(w, "ERROR %s", err)
 		return
 	}
+}
+
+// NOTE: I am too lazy to write my own golang eval;
+// or maybe I just haven't seen a great example online & don't want to figure it out
+// either way, we are going to redirect the request to tour.golang.org/compile
+func handleCompile(w http.ResponseWriter, r *http.Request) {
+	client := &http.Client{}
+	body, _ := r.Body.(io.ReadCloser)
+	req := newProxyRequest(r.Header, *r.URL, r.Method, body)
+	resp, _ := client.Do(req)
+	defer resp.Body.Close()
+
+	for key, _ := range resp.Header {
+		w.Header().Add(key, resp.Header.Get(key))
+	}
+
+	w.WriteHeader(resp.StatusCode)
+	bodyBytes, _ := ioutil.ReadAll(resp.Body)
+	writeBody(w, bodyBytes)
+
+	fmt.Printf("Proxy received %d %s %s\n", resp.StatusCode, r.Method, r.URL.Path)
+}
+
+func newProxyRequest(origHeader http.Header, url url.URL, method string, body io.ReadCloser) (req *http.Request) {
+	urlStr := "http://tour.golang.org/" + url.String()
+	u, _ := url.Parse(urlStr)
+	var header = make(http.Header)
+	for key, _ := range origHeader {
+		header.Add(key, origHeader.Get(key))
+	}
+	req = &http.Request{
+		Method:     method,
+		URL:        u,
+		Proto:      "HTTP/1.1",
+		ProtoMajor: 1,
+		ProtoMinor: 1,
+		Header:     header,
+		Host:       u.Host,
+		Body:       body,
+	}
+	return
+}
+
+func writeBody(w http.ResponseWriter, bodyBytes []byte) {
+	body := string(bodyBytes)
+	fmt.Fprint(w, body)
 }
